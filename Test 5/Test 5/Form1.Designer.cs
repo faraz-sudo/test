881ff54576e9f29dc8f0c9d72e1da8a027c9d27b
faraz-sudo <lvl9999mafiaboss@gmail.com>
2025-08-24 23:52:25 -0400

    Add files via upload

---
+++ Test 5/Test 5/Form1.Designer.cs
@@ -0,0 +1,43 @@
+﻿namespace Test_5
+{
+    partial class firmTimeForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label lblTime;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lblTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+
+            // lblTime
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTime.Location = new System.Drawing.Point(50, 50);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(200, 45);
+            this.lblTime.TabIndex = 0;
+            this.lblTime.Text = "00:00:00";
+
+            // firmTimeForm
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 150);
+            this.Controls.Add(this.lblTime);
+            this.Name = "firmTimeForm";
+            this.Text = "Digital Clock";
+            this.Load += new System.EventHandler(this.firmTimeForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
